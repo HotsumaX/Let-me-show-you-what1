a867cad71185fec5ecabef83c5ba51385327336d
HotsumaX <luislaborde@gmail.com>
2019-06-28 15:13:27 -0400

    modified title, favicon, and started building out the form

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+const styles = {
+  main: {
+    border: '1px #FD435B solid',
+    height: '40em',
+    width: '60em',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+};
+
 const Form = () => {
-  return <div>form</div>;
+  return (
+    <div style={styles.main}>
+      <div>here is some stuff</div>
+    </div>
+  );
 };
 
 export default Form;
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import faker from 'faker';
+
+const {
+  name: { firstName, lastName },
+  address: { zipCode, city },
+  lorem: { paragraph },
+} = faker;
 
 const Home = () => {
-  return <div>home stuff goes here</div>;
+  return (
+    <div>
+      home stuff goes here
+      <div>{firstName()}</div>
+      <div>{lastName()} </div>
+      <div>{zipCode()}</div>
+      <div> {city()}</div>
+      <div>{paragraph()}</div>
+    </div>
+  );
 };
 
 export default Home;
