d58efb6bade457ad64dca9e2451463ee85a7c2b6
HotsumaX <luislaborde@gmail.com>
2019-06-27 22:55:26 -0400

    setting up the header and footer of the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import { render } from "react-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const styles = {
+  NavBar:{
+    fontSize: 30,
+    backgroundColor:"green",
+    height:70
+  },
+  BotSec:{
+    backgroundColor:"green",
+    position:"absolute",
+    left:0,
+    bottom:0,
+    right:0,
+    height:70
+  }
+}
+ const NavBar = () =>{
+          return <div style={styles.NavBar}>something</div>
+        }
+
+
+const BotSec = () =>{
+  return <div style={styles.BotSec}>something else</div>
+}
+
+
+const App = () =>{
+  return (
+    <>
+<NavBar/>
+    <body>
+      stuff
+    </body>
+    <BotSec/>
+    </>
+  )
+}
+
+render(<App/>,document.getElementById('root'))
