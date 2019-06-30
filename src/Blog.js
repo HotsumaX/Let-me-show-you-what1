fc464c66091e7289d39a08c1c6cbd147510d9a9f
HotsumaX <luislaborde@gmail.com>
2019-06-30 13:46:37 -0400

    added the first layout of the blog

--- src/Blog.js
+++ src/Blog.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import faker from 'faker';
+
+const styles = {
+  outline: {
+    backgroundColor: '#2A79D3',
+    width: '90%',
+    marginLeft: '5%',
+    display: 'flex',
+    height: '70em',
+    justifyContent: 'center',
+  },
+  post: {
+    marginTop: 30,
+    width: '50rem',
+    backgroundColor: 'white',
+    marginLeft: 30,
+  },
+};
+
+const article = {
+  Name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+};
 
 const Blog = () => {
-  return <div>blog</div>;
+  return (
+    <div style={styles.outline}>
+      <div style={styles.post}>{article.Name}</div>
+    </div>
+  );
 };
 
 export default Blog;
