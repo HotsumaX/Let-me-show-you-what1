d8799299be769bd9ce0484f0cb203a36f9e49fd3
HotsumaX <luislaborde@gmail.com>
2019-07-12 07:20:17 -0400

    added the switch button and testing sizing

--- src/Dash/ProfileCard/Actions.js
+++ src/Dash/ProfileCard/Actions.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Switch from 'react-switch';
 
 const Actions = () => {
+  const [follButt, setFollButt] = useState(true);
   return (
     <div>
       <div>Actions</div>
-      <div>Follows button</div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Switch
+          handleDiameter={15}
+          height={20}
+          width={40}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          offColor="#D2E6F6"
+          onColor="#0986FF"
+          checked={follButt}
+          onChange={() => setFollButt(!follButt)}
+        />
+        Follows button
+      </div>
       <div>unfollows button</div>
 
       <div>likes button</div>
