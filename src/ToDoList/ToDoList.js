0a0f705e637ac03ef3ad56e63523368b0df6fe28
HotsumaX <luislaborde@gmail.com>
2019-07-23 15:29:43 -0400

    adding some more final tweeks to clean up code errors

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -5,7 +5,7 @@ import './index.css';
 const ToDoList = () => {
   const [items, setItems] = useState([]);
   const [currentItem, setCurrentItem] = useState({ text: '', key: '' });
-  const inputElement = useRef();
+  const inputElement = useRef(null);
 
   useEffect(() => {
     inputElement.current.focus();
@@ -18,8 +18,39 @@ const ToDoList = () => {
 
   const addItem = event => {
     event.preventDefault();
-    console.log('hello add input');
+    const newItem = currentItem;
+    if (newItem.text !== '') {
+      console.log(newItem);
+      const item = [...items, newItem];
+      setItems(item);
+      setCurrentItem({ text: '', key: '' });
+    }
   };
+
+  const TodoItems = props => {
+    const createTasks = item => {
+      return (
+        <div
+          onClick={() => props.deleteItem(item.key)}
+          onKeyPress={() => props.deleteItem(item.key)}
+          role="button"
+          tabIndex={0}
+        >
+          <li key={item.key}>{item.text}</li>
+        </div>
+      );
+    };
+    const todoEntries = props.entries;
+    const listItems = todoEntries.map(createTasks);
+
+    return <ul className="theList">{listItems}</ul>;
+  };
+
+  const deleteItem = key => {
+    const filteredItems = items.filter(item => item.key !== key);
+    setItems(filteredItems);
+  };
+
   return (
     <div className="app-body">
       <div className="todoListMain">
@@ -36,6 +67,7 @@ const ToDoList = () => {
           </form>
         </div>
       </div>
+      <TodoItems entries={items} deleteItem={deleteItem} />
     </div>
   );
 };
