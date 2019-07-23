4636123bac4958fb6e1b6994a8fc5909832b8641
HotsumaX <luislaborde@gmail.com>
2019-07-23 11:02:49 -0400

    added the useRef, useEffect and another useState components to the project

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import './index.css';
 
 const ToDoList = () => {
-  return <div className="app-body">todolist</div>;
+  const [items, setItems] = useState([]);
+  const [currentItem, setCurrentItem] = useState({ text: '', key: '' });
+  const inputElement = useRef();
+
+  useEffect(() => {
+    inputElement.current.focus();
+  });
+
+  const handleInput = event => {
+    const itemText = event.target.value;
+    setCurrentItem({ text: itemText, key: Date.now() });
+  };
+
+  const addItem = event => {
+    event.preventDefault();
+    console.log('hello add input');
+  };
+  return (
+    <div className="app-body">
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={addItem}>
+            <input
+              type="text"
+              placeholder="Task"
+              ref={inputElement}
+              value={currentItem.text}
+              onChange={handleInput}
+            />
+            <button type="submit">Add Task</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ToDoList;
