e9ce853e794dacfc0d462fee6abe894b7e7f37f9
HotsumaX <luislaborde@gmail.com>
2019-07-26 10:01:07 -0400

    adding more scaffolding of footer components

---
+++ src/Footer/FooterCSS.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './Footer.css';
+
+const FooterCSS = () => {
+  return <div>footer 1</div>;
+};
+export default FooterCSS;
---
+++ src/Footer/FooterReact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './Footer.css';
+
+const FooterReact = () => {
+  return <div>footer 2</div>;
+};
+export default FooterReact;
